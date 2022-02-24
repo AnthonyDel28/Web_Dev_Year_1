5e273223633a507725829c6e9260dd006855b040
AnthonyDel28 <anthonydelmeire2709@gmail.com>
2022-02-24 11:57:53 +0100

    update 24_02_2022

---
+++ script_server/cours/01-array.php
@@ -0,0 +1,9 @@
+<?php
+
+$count = 10;
+
+for($i = 1; $i <= $count; $i++){
+    echo "Tout fonctionne correctement!<br>";
+}
+
+?>
--- script_server/cours/02-members.php
+++ script_server/cours/02-members.php
@@ -3,3 +3,4 @@
 
 // 1) Inclure le fichier "members-array.php" et affichez son contenu dans un tableau HTML.  La première colonne contiendra une numérotation.  Les membres seront affichés par ordre alphabétique.  Les en-têtes du tableau (titres) seront également affichés à l'aide d'une boucle.  Mettre une majuscule aux étiquettes.
 
+echo "Hello";
