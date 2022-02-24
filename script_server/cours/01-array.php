1d3fea267ec59b61229887d7f200f5942d9f4490
AnthonyDel28 <anthonydelmeire2709@gmail.com>
2022-02-24 13:09:05 +0100

    update 24_02_2022

--- script_server/cours/01-array.php
+++ script_server/cours/01-array.php
@@ -1,9 +1,11 @@
 <?php
+// Exercices tableaux
+$courses = ['HTML' => 80, 'CSS' => 60, 'PHP' => 125, 'JS' => 100, 'Symfony' => 120, 'Design' => 80];
 
-$count = 10;
-
-for($i = 1; $i <= $count; $i++){
-    echo "Tout fonctionne correctement!<br>";
-}
-
-?>
+// 1. Ajoutez dans le tableau le cours "Network 40" (sans modifier manuellement le tableau)
+// 2. Affichez le nombre de périodes du cours de CSS
+// 3. Vérifiez si le cours de Symfony est bien dans le tableau.  Le résultat devrait être la valeur numérique 1
+// 4. A l'aide d'une boucle, affichez tous les cours triés par périodes (croissant)
+// 5. A l'aide d'une boucle, affichez les cours dont les périodes sont comprises entre 100 et 120
+// 6. Affichez la moyenne des périodes et arrondissez le résultat
+// 7. Convertissez le tableau associatif en variables et ajoutez 20 périodes au cours HTML.  Affichez le nouveau résultat.
--- script_server/cours/02-members.php
+++ script_server/cours/02-members.php
@@ -1,6 +1,11 @@
 <?php
-// Rappels et manipulations des tableaux
+// 1. manipulations des tableaux multidimensionnels
 
-// 1) Inclure le fichier "members-array.php" et affichez son contenu dans un tableau HTML.  La première colonne contiendra une numérotation.  Les membres seront affichés par ordre alphabétique.  Les en-têtes du tableau (titres) seront également affichés à l'aide d'une boucle.  Mettre une majuscule aux étiquettes.
+// 1.1 Inclure le fichier "members-array.php"
+// 1.2 Affichez son contenu dans un tableau HTML.
+// 1.3 La première colonne du tableau contiendra une numérotation.
+// 1.4 Les membres seront affichés par ordre alphabétique (sur le nom de famille).
+// 1.5 Les en-têtes du tableau (titres) seront également affichés à l'aide d'une boucle.
+// 1.6 Mettre une majuscule aux étiquettes (fonction en PHP)
+// 1.7 Colorier l'arrière plan d'une ligne sur deux.  Utiliser l'opérateur Modulo en PHP.
 
-echo "Hello";
