cdc3eb5e148be78314b3792dec749a51ec3c246e
AnthonyDel28 <anthonydelmeire2709@gmail.com>
2022-02-28 23:36:12 +0100

    update 28_02_2022

--- script_server/cours/02-members.php
+++ script_server/cours/02-members.php
@@ -9,3 +9,65 @@
 // 1.6 Mettre une majuscule aux étiquettes (fonction en PHP)
 // 1.7 Colorier l'arrière plan d'une ligne sur deux.  Utiliser l'opérateur Modulo en PHP.
 
+include('./sources/members-array.php');
+// require('./sources/members-array.php');
+
+
+?>
+
+<table>
+    <thead>
+        <tr>
+            <?php 
+                    print "<th>N.</th>";
+                    foreach($members as $tab)
+                    {
+                        foreach($tab as $key => $value)
+                        {
+                            echo "<th>".ucfirst($key)."</th>";
+                        }
+                        break;
+                    }   
+            ?>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <?php 
+                    $index = 1;
+                        foreach($members as $tab){
+                                if($index % 2 == 1)
+                                    print "<tr class='line1'>";
+                                print "<td>" .$index. "</td>";
+                                foreach($tab as $key => $value){
+                                    print "<td>" .$value . "</td>";
+                                }
+                                $index++;
+                            print "</tr>";
+                        }
+            ?>
+        </tr>
+    </tbody>
+</table>
+
+<style>
+table,
+td {
+    border: 1px solid #333;
+}
+
+th {
+    background-color: #333;
+    color: #fff;
+}
+
+.line1 {
+    background-color: #d5445d;
+}
+
+</style>
+
+
+
+
+
