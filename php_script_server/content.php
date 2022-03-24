48c363665fa6300d9789fbb4bded430a26efc114
AnthonyDel28 <anthonydelmeire2709@gmail.com>
2022-03-24 12:08:41 +0100

    update 24_03_2022

--- php_script_server/content.php
+++ php_script_server/content.php
@@ -4,8 +4,8 @@ if(!empty($view)){
     $path = __DIR__ . '/view/' . $view .'.html';
     if(file_exists($path)){
         include_once $path;
-    } else {
-        header('Location: index.php');
         die;
     }
 }
+
+header('Location: index.php?view=main');
---
+++ php_script_server/view/play.php
@@ -0,0 +1,10 @@
+<?php
+
+
+$number = mt_rand(0, 100);
+print "Votre valeur alétaoire entre 0 et 100 est: ".$number."<br>";
+
+?>
+
+<a href="?view=play">Rejouer</a>
+
