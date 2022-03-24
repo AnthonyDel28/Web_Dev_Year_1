f2d0cf24201ac189f0d6731e5c96a17b2f087174
AnthonyDel28 <anthonydelmeire2709@gmail.com>
2022-03-24 14:18:20 +0100

    update 24_03_2022

--- php_script_server/content.php
+++ php_script_server/content.php
@@ -1,10 +1,15 @@
 <?php
 
+$exts = ['html', 'php'];
+
 if(!empty($view)){
-    $path = __DIR__ . '/view/' . $view .'.html';
-    if(file_exists($path)){
-        include_once $path;
-        die;
+    $path = __DIR__ . '/view/' . $view;
+    foreach($exts as $ext){
+        $complete_path = $path . '.' . $ext;
+        if(file_exists($complete_path)){
+            include_once $complete_path;
+            die;
+        }
     }
 }
 
