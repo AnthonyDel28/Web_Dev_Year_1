8d7651fe925b294546c2cd3b031b0e50f03c9439
AnthonyDel28 <anthonydelmeire2709@gmail.com>
2022-04-21 13:54:42 +0200

    update 21_04_2022

--- script_server/PDO/index.php
+++ script_server/PDO/index.php
@@ -3,10 +3,39 @@
 
 $dbh = new PDO('mysql:host=localhost;dbname=Mediarnaque', 'root', 'root');
 
-
-$result = $dbh->query("SELECT * FROM laptop WHERE idL = 10");
+/*
+$result = $dbh->query("SELECT * FROM vcard");
 $data = $result->fetch(PDO::FETCH_ASSOC);
 foreach($data as $key => $value){
-    print $key . ' ' . $value . '<br>';
+    print $value[] . '<br>';
+}*/
+
+/* $sth = $dbh->query("SELECT nameB, modelV FROM vcard INNER JOIN brand ON vcard.brandV = brand.idB");
+
+ $result = $sth->fetchAll();
+ foreach($result as $key => $value){
+    print $value['nameB'] . ' ' . $value['modelV'] . '<br>';
+} */
+
+
+#-----------------------------------------------------------------
+
+$order = "DESC";
+$sql_query = 'SELECT DISTINCT *,
+                b.nameB as marque, 
+                CONCAT(bcpu.nameB," ",modelCpu) as namecpu, 
+                bvcard.nameB as brandvcard
+        FROM laptop as l, brand as b, brand as bcpu, brand as bvcard, cpu as c, vcard as v, color as col 
+        WHERE b.idB=l.brandL AND c.idCpu=l.cpuL AND v.idV=l.vcardL AND bcpu.idB=c.brandCpu AND bvcard.idB=v.brandV AND col.idC=l.colorL';
+
+$result = $dbh->query($sql_query);
+
+foreach($result as $data){
+    print $data['marque'] . ' ' . $data['namecpu'] . ' ' . $data['brandvcard'] . '<br>';
 }
+
+
+
+
 ?>
+
