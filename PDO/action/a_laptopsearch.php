24932f0ba3096b2928e25c1ad322e9b34ddf9ce6
AnthonyDel28 <anthonydelmeire2709@gmail.com>
2022-04-26 20:43:53 +0200

    update 26_04_2022

--- PDO/action/a_laptopsearch.php
+++ PDO/action/a_laptopsearch.php
@@ -11,38 +11,5 @@
 // Initialisation de la connexion à la BDD
 $connect = connect();
 
-foreach($_POST as $array => $sub){
-    foreach($sub as $key => $value){
-        $data[] = $value;
-    }
-}
-$weight1 = substr($data[6], 0, 4);
-$weight2 = substr($data[6], 5);
-unset($data[6]);
-$data[] = $weight1;
-$data[] = $weight2;
-$data_string = implode(", ", $data);
-$sql = "SELECT brand.nameB as `Marque`,
-       color.nameC as `Couleur`,
-       cpu.modelCpu as `Processeur`,
-       laptop.ramL as `Ram`,
-       laptop.screenL as `Taille d'écran`,
-       vcard.modelV as `Carte graphique`,
-       laptop.weightL as `Poids`,
-       laptop.priceL as `Prix`
-FROM laptop
-         JOIN brand ON laptop.brandL = brand.idB
-         JOIN vcard ON laptop.vcardL = vcard.idV
-         JOIN color ON laptop.colorL = color.idC
-         JOIN cpu ON laptop.cpuL = cpu.idCpu
-WHERE laptop.brandL = ? AND laptop.colorL = ? AND laptop.cpuL = ? AND laptop.ramL = ? AND laptop.screenL = ? AND laptop.vcardL = ? AND laptop.priceL < ? AND laptop.weightL BETWEEN ?  AND ?";
-
-
-$stmt = $connect->prepare($sql);
-$stmt->execute([$data[0], $data[1], $data[2], $data[3], $data[4], $data[5], $data[7], $data[8], $data[9]]);
-$stmt->setFetchMode(PDO::FETCH_OBJ);
-
-foreach ($stmt as $row) {
-    var_dump($row);
-}
+print_r($_POST);
 
