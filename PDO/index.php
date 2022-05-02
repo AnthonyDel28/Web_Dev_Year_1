0230b3494d27bf04835cc1d40d37ea91ae1cb29e
AnthonyDel28 <anthonydelmeire2709@gmail.com>
2022-05-02 14:43:01 +0200

    update 02_05_2022

--- PDO/index.php
+++ PDO/index.php
@@ -1,21 +1,47 @@
 <?php
 
-// Inclusion des scripts contenant les fonctions utiles
-require_once __DIR__ . '/lib/database.php';
-require_once __DIR__ . '/form/formlib.php';
-require_once __DIR__ . '/lib/lib.php';
+define('DB_NAME', 'mediarnaque');
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASSWORD', 'root');
 
-// Paramètre d'url gérant les vues
-$view = '';
-if (isset($_GET['view'])) {
-    $view = $_GET['view'];
+function connect() {
+    // variable globale. Si le script appelant la fonction connect() possède une variable nommée $dbh, alors son contenu sera récupéré ici
+    global $dbh;
+
+    // Il est inutile de créer une nouvelle connexion à la DB si elle existe déjà
+    if (!$dbh) {
+        // try / catch : gestion d'erreur. Si le code dans la partie "try" échoue, alors la partie "catch" s'exécute.
+        try {
+            // La connexion PDO utilise les constantes présentes dans le script config.php
+            $dbh = new PDO('mysql:dbname=' . DB_NAME . ';host=' . DB_HOST . ';charset=utf8', DB_USER, DB_PASSWORD);
+            // On spécifie le mode d'erreur de PDO via la méthode setAttribute et les constantes de PDO
+            // Avant PHP 8.0 il est nécessaire de le préciser, car ce n'est pas le mode par défaut
+            $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $exception) {
+            // On affiche l'erreur
+            die ('Erreur: ' . $exception->getMessage());
+        }
+    }
+    return $dbh;
 }
 
-// Connexion à la DB
+
 $connect = connect();
 
-// Inclusion des vues
-include_once __DIR__ . '/view/header.php';
-include_once __DIR__ . '/view/menu.php';
-include_once __DIR__ . '/view/main.php';
-include_once __DIR__ . '/view/footer.php';
+$color = 'violet';
+
+
+$res = $connect->prepare("INSERT INTO color (nameC) VALUES (?)");
+$res->execute([$color]);
+
+$res->setFetchMode(PDO::FETCH_OBJ);
+
+$res2 = $connect->prepare("SELECT * FROM color");
+$res2->execute();
+$res2->setFetchMode(PDO::FETCH_OBJ);
+foreach($res2 as $row){
+        print $row->idC . ' -> ' . $row->nameC . '<br>';
+    }
+
+
